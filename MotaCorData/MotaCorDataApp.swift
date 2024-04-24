2705f834ff3cddfd8963a08d8fd7558c350ee396
Sam Hastings <samhastingsis@gmail.com>
2024-04-24 15:32:36 -0400

    Initial Commit

---
+++ MotaCorData/MotaCorDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotaCorDataApp.swift
+//  MotaCorData
+//
+//  Created by sam hastings on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MotaCorDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
